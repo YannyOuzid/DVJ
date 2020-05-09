43ba6789fd4988f34cc184c8580deaca3ecb2e9a
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-05-10 01:53:10 +0200

    project

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Commentaire;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,9 +16,11 @@ class AdminController extends AbstractController
     {
         $em = $this->getDoctrine()->getManager();
         $users = $em->getRepository(User::class)->findAll();
+        $commentaires = $em->getRepository(Commentaire::class)->findAll();
 
         return $this->render('admin/index.html.twig', [
             'users' => $users,
+            'commentaires' => $commentaires
         ]);
     }
 
